9e2fd740c01e5dd63a3379a1bc737fa8ca08e609
Gabriel Paul <gpaulbr@gmail.com>
2016-11-24 11:26:36 -0200

    Adiciona estrutura de repetição

--- categorias.py
+++ categorias.py
@@ -10,7 +10,8 @@ def adiciona_tag(entidade, texto, inicio):
     sub_texto_inicial = texto[:inicio]
     sub_texto_final = texto[inicio:]
     texto = sub_texto_inicial + '<EM ID="' + arquivo[10:-4] + '" CATEG="baciaSEDIMENTAR">' + sub_texto_final
-    return texto
+    inicio = len(sub_texto_inicial + '<EM ID="' + arquivo[10:-4] + '" CATEG="baciaSEDIMENTAR">')
+    return texto, inicio
 
 
 with open("Bacias.txt") as bacias_arquivo:
@@ -22,14 +23,18 @@ with open("Formacoes.txt") as formacoes_arquivo:
 with open("Granulometrias.txt") as granulometrias_arquivo:
     granulometrias = granulometrias_arquivo.read()[:-1].split("\n")
 
+inicio = 0
 for arquivo in arquivos:
     with open(arquivo) as texto:
         texto = texto.read()
-        inicio = texto.find("Bacia")
-        for bacia in bacias:    
-            if bacia in texto[inicio:(inicio+len(bacia))]:
-                print bacia
-                texto = adiciona_tag(bacia, texto, inicio)
+        print texto[:1000]
+        while inicio != -1:
+            inicio = texto.find("Bacia", inicio)
+            print inicio
+            for bacia in bacias:    
+                if bacia in texto[inicio:(inicio+len(bacia))]:
+                    print bacia
+                    texto, inicio = adiciona_tag(bacia, texto, inicio)
         
 
         file = open("./saida/" + arquivo[10:], 'w')
