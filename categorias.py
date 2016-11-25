63ac78c471a8f6a4295f61b6b81501a47ddc616b
Gabriel Paul <gpaulbr@gmail.com>
2016-11-25 10:17:48 -0200

    Modifica lógica para achar todos os índices primeiro

--- categorias.py
+++ categorias.py
@@ -9,9 +9,37 @@ def adiciona_tag(entidade, texto, inicio):
     texto = sub_texto_inicial + '</EM>' + sub_texto_final
     sub_texto_inicial = texto[:inicio]
     sub_texto_final = texto[inicio:]
-    texto = sub_texto_inicial + '<EM ID="' + arquivo[10:-4] + '" CATEG="baciaSEDIMENTAR">' + sub_texto_final
-    inicio = len(sub_texto_inicial + '<EM ID="' + arquivo[10:-4] + '" CATEG="baciaSEDIMENTAR">')
-    return texto, inicio
+    texto = sub_texto_inicial + '<EM ID="' + arquivo[10:-4] + '" CATEG="granulometria">' + sub_texto_final
+
+    return texto
+
+
+def encontra_todos(texto, categoria):
+    inicio = 0
+    while True:
+        inicio = texto.find(categoria, inicio)
+        if inicio == -1: return
+        yield inicio
+        inicio += len(categoria)
+
+
+def processa_categoria(texto, entidades, categoria=None):
+    if categoria is None:
+        for entidade in entidades:
+            print entidade
+            lista = list(encontra_todos(texto, entidade))
+            while len(lista) > 0:
+                texto = adiciona_tag(entidade, texto, lista.pop())
+                # print len(lista)
+    else:
+        lista = list(encontra_todos(texto, categoria))
+        while len(lista) > 0:
+            inicio = lista.pop()
+            # print len(lista)
+            for entidade in entidades:    
+                if entidade in texto[inicio:(inicio+len(entidade))]:
+                    texto = adiciona_tag(entidade, texto, inicio)
+    return texto
 
 
 with open("Bacias.txt") as bacias_arquivo:
@@ -23,19 +51,14 @@ with open("Formacoes.txt") as formacoes_arquivo:
 with open("Granulometrias.txt") as granulometrias_arquivo:
     granulometrias = granulometrias_arquivo.read()[:-1].split("\n")
 
-inicio = 0
 for arquivo in arquivos:
     with open(arquivo) as texto:
         texto = texto.read()
-        print texto[:1000]
-        while inicio != -1:
-            inicio = texto.find("Bacia", inicio)
-            print inicio
-            for bacia in bacias:    
-                if bacia in texto[inicio:(inicio+len(bacia))]:
-                    print bacia
-                    texto, inicio = adiciona_tag(bacia, texto, inicio)
-        
+        for i in granulometrias:
+            print i
+        texto = processa_categoria(texto, bacias, "Bacia ")
+        texto = processa_categoria(texto, formacoes, "Formação ")
+        texto = processa_categoria(texto, granulometrias)
 
         file = open("./saida/" + arquivo[10:], 'w')
         file.write(texto)
